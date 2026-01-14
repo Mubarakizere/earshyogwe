8702d4a567a0e6c7dd33aa32f0df494f5c8835fe
Izere Moubarak <izeremubarak05@gmail.com>
2026-01-14 13:10:18 +0200

    make view deleted button red and visible

--- resources/views/workers/index.blade.php
+++ resources/views/workers/index.blade.php
@@ -13,7 +13,7 @@
             
             <div class="flex gap-2">
                 @can('delete worker')
-                    <a href="{{ route('workers.trashed') }}" class="inline-flex items-center px-4 py-2 bg-gray-600 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700">
+                    <a href="{{ route('workers.trashed') }}" class="inline-flex items-center px-4 py-2 bg-red-600 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-red-500">
                         <svg class="w-4 h-4 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"></path>
                         </svg>
